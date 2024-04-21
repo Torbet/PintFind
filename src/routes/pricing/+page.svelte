85c2d9692724a1442f78136e91da74ad70895709
Guy Torbet <guy@torbet.co>
2024-04-21 22:58:44 +0100

    Pricing

---
+++ src/routes/pricing/+page.svelte
@@ -0,0 +1,290 @@
+<svelte:head>
+	<title>Pricing | PintFind</title>
+</svelte:head>
+
+<section class="grid gap-4 lg:grid-cols-3">
+	<!-- Essential -->
+	<div class="flex flex-col gap-4 rounded-box border p-6">
+		<h2 class="text-2xl font-bold">Essential</h2>
+
+		<span class="text-lg text-gray-500">Get Found and Attract More Locals</span>
+
+		<div class="flex items-end">
+			<span class="text-4xl font-bold">£12.99</span>
+			<span class="text-xl font-bold text-gray-600">/month</span>
+		</div>
+
+		<ul class="flex flex-col gap-6">
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Add social media links
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Upload photos to showcase your venue
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Share events and promotions
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Select menu highlights
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Add opening hours & contact details
+			</li>
+		</ul>
+		<button class="btn btn-primary mt-auto">Get Started</button>
+	</div>
+
+	<!-- Boost -->
+	<div class="flex flex-col gap-4 rounded-box border p-6">
+		<h2 class="text-2xl font-bold">Boost</h2>
+
+		<span class="text-lg text-gray-500">Stand Out as a Local Hotspot</span>
+
+		<div class="flex items-end">
+			<span class="text-4xl font-bold">£24.99</span>
+			<span class="text-xl font-bold text-gray-600">/month</span>
+		</div>
+
+		<span class="text-lg text-gray-500">Everything in Essential, plus:</span>
+
+		<ul class="flex flex-col gap-6">
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Get recommended in your area
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Verified badge to increase credibility
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Run promotions and offers
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Display booking details
+			</li>
+		</ul>
+		<button class="btn btn-primary mt-auto">Get Started</button>
+	</div>
+
+	<!-- Enterprise -->
+	<div class="flex flex-col gap-4 rounded-box border p-6">
+		<h2 class="text-2xl font-bold">Enterprise</h2>
+
+		<span class="text-lg text-gray-500">Amplify Your Brand's Reach</span>
+
+		<div class="flex items-end">
+			<span class="text-4xl font-bold">Contact Us</span>
+		</div>
+
+		<span class="text-lg text-gray-500">Everything in Essential, plus:</span>
+
+		<ul class="flex flex-col gap-6">
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Deals and offers in your customers inbox
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Premium ad placement site-wide
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Personalised marketing campaigns
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Advances analtics and reporting
+			</li>
+		</ul>
+		<button class="btn btn-primary mt-auto">Get Started</button>
+	</div>
+</section>
