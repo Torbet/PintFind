39ca865fbaca2f9ae34271f41441faf5032bfb28
Guy Torbet <guy@torbet.co>
2024-04-21 17:22:06 +0100

    More landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -71,6 +71,72 @@
 	</a>
 </section>
 
+<section class="grid grid-cols-1 gap-4 md:grid-cols-3">
+	<div class="flex flex-col items-center gap-4 rounded-box border p-6 text-center">
+		<svg
+			width="52"
+			height="52"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+			/>
+		</svg>
+		<h3 class="text-xl font-bold">Top Rated Bars & Brews</h3>
+		<p class="text-lg text-gray-600">Explore the best rated beers and pubs - wherever you are!</p>
+	</div>
+
+	<div class="flex flex-col items-center gap-4 rounded-box border p-6 text-center">
+		<svg
+			width="52"
+			height="52"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
+			/>
+		</svg>
+		<h3 class="text-xl font-bold">Join The Community</h3>
+		<p class="text-lg text-gray-600">
+			Join our community of beer lovers and share your favourites or discover something new!
+		</p>
+	</div>
+
+	<div class="flex flex-col items-center gap-4 rounded-box border p-6 text-center">
+		<svg
+			width="52"
+			height="52"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
+			/>
+		</svg>
+		<h3 class="text-xl font-bold">Awesome Deals</h3>
+		<p class="text-lg text-gray-600">
+			With the most up to date prices and deals, we help you to save money!
+		</p>
+	</div>
+</section>
+
 <div class="divider"></div>
 
 <section class="flex flex-col gap-4">
