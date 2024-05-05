b2c39156e04a83d8e9e735fcf4aadef5ad0e587f
Guy Torbet <guy@torbet.co>
2024-05-05 13:37:58 +0100

    Add leaderboard to nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -72,6 +72,29 @@
 						Review
 					</a>
 				</li>
+				<li>
+					<a href="/leaderboard">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="1.5"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							><path
+								d="M7.21 15 2.66 7.14a2 2 0 0 1 .13-2.2L4.4 2.8A2 2 0 0 1 6 2h12a2 2 0 0 1 1.6.8l1.6 2.14a2 2 0 0 1 .14 2.2L16.79 15"
+							/><path d="M11 12 5.12 2.2" /><path d="m13 12 5.88-9.8" /><path d="M8 7h8" /><circle
+								cx="12"
+								cy="17"
+								r="5"
+							/><path d="M12 18v-2h-.5" /></svg
+						>
+						Leaderboard
+					</a>
+				</li>
 			</ul>
 			<div class="navbar-end">
 				<div class="hidden gap-2 lg:flex">
@@ -187,6 +210,29 @@
 						/>
 					</svg>
 					Review
+				</a>
+			</li>
+			<li>
+				<a href="/leaderboard">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						><path
+							d="M7.21 15 2.66 7.14a2 2 0 0 1 .13-2.2L4.4 2.8A2 2 0 0 1 6 2h12a2 2 0 0 1 1.6.8l1.6 2.14a2 2 0 0 1 .14 2.2L16.79 15"
+						/><path d="M11 12 5.12 2.2" /><path d="m13 12 5.88-9.8" /><path d="M8 7h8" /><circle
+							cx="12"
+							cy="17"
+							r="5"
+						/><path d="M12 18v-2h-.5" /></svg
+					>
+					Leaderboard
 				</a>
 			</li>
 			{#if user}
