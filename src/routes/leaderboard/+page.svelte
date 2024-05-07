d6ce267b1398d6d9b5bfdc3aaa367bb4ad2289c0
Guy Torbet <guy@torbet.co>
2024-05-07 19:14:00 +0100

    Medal on leaderboard

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -37,7 +37,29 @@
 			{#each topUsers as { username, reviewCount }, i}
 				<tr on:click={() => goto(`/users/${username}`)} class="hover cursor-pointer">
 					<td class="font-bold">{i + 1}</td>
-					<td>{username}</td>
+					<td class="flex items-center gap-2">
+						{#if i == 0}
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								><path
+									d="M7.21 15 2.66 7.14a2 2 0 0 1 .13-2.2L4.4 2.8A2 2 0 0 1 6 2h12a2 2 0 0 1 1.6.8l1.6 2.14a2 2 0 0 1 .14 2.2L16.79 15"
+								/><path d="M11 12 5.12 2.2" /><path d="m13 12 5.88-9.8" /><path d="M8 7h8" /><circle
+									cx="12"
+									cy="17"
+									r="5"
+								/><path d="M12 18v-2h-.5" /></svg
+							>
+						{/if}
+						{username}
+					</td>
 					<td>{reviewCount}</td>
 				</tr>
 			{/each}
