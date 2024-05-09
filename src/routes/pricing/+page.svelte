03079e1f8b79f1d0d153be24bf89396c3a7ce509
Guy Torbet <guy@torbet.co>
2024-05-09 21:18:38 +0100

    Update pricing

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -19,18 +19,125 @@
 	<section class="mb-6 flex flex-col gap-4">
 		<h2 class="text-4xl font-semibold">Always Free for Beer Lovers</h2>
 		<p class="text-lg text-gray-500">
-			PintFind is, and always will be, completely free for beer enthusiasts to use.
+			PintFind is, and always will be, completely free for everyone to use.
 		</p>
 		<p class="text-lg text-gray-500">
 			Our mission is to connect passionate beer lovers with the best breweries, pubs, and taprooms,
 			without any barriers or costs for our users.
 		</p>
 		<p class="text-lg text-gray-500">
-			Explore, discover, and share your love of craft beer without worrying about fees or
-			subscriptions.
+			But, if you would like to support us and get some cool perks, you can become a verified user.
 		</p>
 		<p class="text-lg text-gray-500"></p>
 	</section>
+
+	<!-- Boost -->
+	<div
+		class="mx-auto flex w-full flex-col gap-4 rounded-box border border-primary p-6 lg:max-w-2xl"
+	>
+		<h2 class="flex items-center gap-2 text-2xl font-bold">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="28"
+				height="28"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				><path
+					d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"
+				/><path d="m9 12 2 2 4-4" /></svg
+			>
+			Get Verified
+		</h2>
+
+		<span class="text-lg text-gray-500"> Show your love & support by verifying your account. </span>
+
+		<div class="flex items-end">
+			<span class="mr-2 text-2xl font-semibold line-through">£14.99</span>
+			<span class="text-4xl font-bold text-primary">£8.99</span>
+		</div>
+
+		<ul class="flex flex-col gap-6">
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Get a verified badge on your profile
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Your reviews will be highlighted
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Access to exclusive events and offers
+			</li>
+			<li class="flex gap-2">
+				<svg
+					width="28"
+					height="28"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="green"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				Early access to new features
+			</li>
+		</ul>
+		<a href="https://buy.stripe.com/5kA4jTb7reFN4Wk3cc" class="btn btn-primary mt-auto"
+			>Get Started</a
+		>
+	</div>
 {:else if tab == 1}
 	<section class="mb-6 flex flex-col gap-4">
 		<h1 class="text-4xl font-bold">Reach More Beer Lovers with PintFind</h1>
