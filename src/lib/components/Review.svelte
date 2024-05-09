7edde9257454c07769e82e1d2137effb726619b7
Guy Torbet <guy@torbet.co>
2024-05-09 22:28:51 +0100

    Verified

--- src/lib/components/Review.svelte
+++ src/lib/components/Review.svelte
@@ -23,6 +23,7 @@
 
 <a
 	href="/places/{place.slug}"
+	class:border-primary={user.verified}
 	class="flex grow flex-col gap-2 rounded-box border p-6 hover:bg-base-200"
 >
 	<a href="/users/{user.username}" class="flex gap-2">
@@ -36,7 +37,25 @@
 			</div>
 		</div>
 		<div class="flex flex-col">
-			<span class="font-semibold">{user.username}</span>
+			<span class="flex items-center gap-1 font-semibold">
+				{user.username}
+				{#if user.verified}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="18"
+						height="18"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						><path
+							d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"
+						/><path d="m9 12 2 2 4-4" /></svg
+					>
+				{/if}
+			</span>
 			<span class="text-sm text-gray-500">Joined {formatDate(user.createdAt)}</span>
 		</div>
 		<span class="ml-auto text-sm text-gray-500">{daysAgoString}</span>
--- src/routes/users/[username]/+page.svelte
+++ src/routes/users/[username]/+page.svelte
@@ -8,7 +8,7 @@
 
 	export let data: PageServerData;
 	const { user, latestReviews, favouritePlaces, favouriteDrinks } = data;
-	const { username, avatar, createdAt, reviewCount } = user;
+	const { username, avatar, createdAt, reviewCount, verified } = user;
 </script>
 
 <svelte:head>
@@ -26,8 +26,26 @@
 				{/if}
 			</div>
 		</div>
-		<div class="flex flex-col gap-1">
-			<span class="text-3xl font-bold">{username}</span>
+		<div class:text-primary={verified} class="flex flex-col gap-1">
+			<span class="flex items-center gap-1 text-3xl font-bold">
+				{username}
+				{#if verified}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						><path
+							d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"
+						/><path d="m9 12 2 2 4-4" /></svg
+					>
+				{/if}
+			</span>
 			<span class="badge badge-outline badge-lg text-xl text-gray-500">{reviewCount} reviews</span>
 			<span class="text-gl text-gray-500">Joined {formatDate(createdAt)}</span>
 		</div>
