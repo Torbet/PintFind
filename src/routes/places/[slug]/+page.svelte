807c1182c08797314ce82d999a6a428b748d7c33
Guy Torbet <guy@torbet.co>
2024-05-10 19:39:23 +0100

    Fixx 500 on place page

--- src/routes/places/[slug]/+page.svelte
+++ src/routes/places/[slug]/+page.svelte
@@ -358,10 +358,12 @@
 <section class=" container flex flex-col gap-4">
 	<h2 class="text-xl font-bold">Menu</h2>
 
-	<div class="flex flex-col">
-		<span class="text-gray-500">Average Price</span>
-		<span class="text-6xl font-bold">{currencySymbol}{avgPrice.toFixed(2)}</span>
-	</div>
+	{#if avgPrice}
+		<div class="flex flex-col">
+			<span class="text-gray-500">Average Price</span>
+			<span class="text-6xl font-bold">{currencySymbol}{avgPrice.toFixed(2)}</span>
+		</div>
+	{/if}
 
 	<div class="overflow-x-auto">
 		<table class="table">
